2e9772c68e71ce55855355f2e13c4ebeee583029
Aryan Hazra <ahazra8@gatech.edu>
2025-02-02 22:41:02 -0500

    updated frontend name

---
+++ website/frontend/src/Pages/chatbot.jsx
@@ -0,0 +1,10 @@
+// src/pages/Chatbot.jsx
+import React from 'react';
+
+const Chatbot = () => (
+  <div className="min-h-screen bg-green-100 flex items-center justify-center">
+    <h1 className="text-4xl font-bold text-green-500">Chatbot Page</h1>
+  </div>
+);
+
+export default Chatbot;
